3f09ac8f1ef91d473520a871d4a27ecfe0893511
xenoxe407-web <xenoxe407@gmail.com>
2026-07-17 21:53:25 +0800

    Initial project setup

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDF1rvPrVjtdrjDmbTKYB3gSK7ussdJAhI",
+  authDomain: "memorial-dff6c.firebaseapp.com",
+  projectId: "memorial-dff6c",
+  storageBucket: "memorial-dff6c.firebasestorage.app",
+  messagingSenderId: "774936683234",
+  appId: "1:774936683234:web:81f2329781e4a2982834bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log("✅ Firebase Connected!");
